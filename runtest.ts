ccb2edc64fda4d81e742e84da083cc5417507d89
정한 Rycont <35295182+rycont@users.noreply.github.com>
2025-05-05 15:59:33 +0900

    Manual Data Update

--- runtest.ts
+++ runtest.ts
@@ -4,18 +4,19 @@ import { getTerminals } from './bustago/get-terminals.ts'
 
 const terminals = await getTerminals()
 
+let i = 0
+
 for (const departingTerminal of terminals.values()) {
     const routes = await getRoutesFromTerminal(departingTerminal.id)
 
     for (const arrivalTerminal of routes) {
-        console.log('-----')
+        console.log(`----- ${i++} / ${terminals.size} -----`)
         console.log(
             `Departure Terminal: ${departingTerminal.name}(${departingTerminal.id})`,
         )
         console.log(
             `Arrival Terminal: ${arrivalTerminal.name}(${arrivalTerminal.id})`,
         )
-        console.log('-----')
         console.log(
             await getPlansFromRoute(departingTerminal.id, arrivalTerminal.id),
         )
