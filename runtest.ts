c1aafca368951e9441f17073359959d7af9db823
정한 Rycont <35295182+rycont@users.noreply.github.com>
2025-05-05 15:50:41 +0900

    chore: upload bustago caches

--- runtest.ts
+++ runtest.ts
@@ -3,20 +3,21 @@ import { getRoutesFromTerminal } from './bustago/get-routes-from-terminal.ts'
 import { getTerminals } from './bustago/get-terminals.ts'
 
 const terminals = await getTerminals()
-const departingTerminal = [...terminals.values()][10]
 
-const routes = await getRoutesFromTerminal(departingTerminal.id)
+for (const departingTerminal of terminals.values()) {
+    const routes = await getRoutesFromTerminal(departingTerminal.id)
 
-for (const arrivalTerminal of routes) {
-    console.log('-----')
-    console.log(
-        `Departure Terminal: ${departingTerminal.name}(${departingTerminal.id})`,
-    )
-    console.log(
-        `Arrival Terminal: ${arrivalTerminal.name}(${arrivalTerminal.id})`,
-    )
-    console.log('-----')
-    console.log(
-        await getPlansFromRoute(departingTerminal.id, arrivalTerminal.id),
-    )
+    for (const arrivalTerminal of routes) {
+        console.log('-----')
+        console.log(
+            `Departure Terminal: ${departingTerminal.name}(${departingTerminal.id})`,
+        )
+        console.log(
+            `Arrival Terminal: ${arrivalTerminal.name}(${arrivalTerminal.id})`,
+        )
+        console.log('-----')
+        console.log(
+            await getPlansFromRoute(departingTerminal.id, arrivalTerminal.id),
+        )
+    }
 }
