88a43c2e5ee131b207be26bfd45de08fb240d46d
rycont <rycont@outlook.kr>
2025-05-16 16:34:01 +0900

    fix: update cache invalidate criteria

--- remove-old-caches.ts
+++ remove-old-caches.ts
@@ -1,23 +1,33 @@
 const DATE_REGEX = /^\d{4}-\d{2}-\d{2}/
 const CACHE_DIR = Deno.env.get('CACHE_DIR') || './cache'
-const TODAY_YYYY_MM_DD = new Date().toISOString().split('T')[0]
 
 export default async function removeOldCaches() {
-    // 생성된 날짜가 오늘이 아닌 캐시 파일을 삭제합니다.
+    // 생성된 날짜가 오늘 날짜보다 이전인 캐시 파일 삭제
     for await (const file of Deno.readDir(CACHE_DIR)) {
         const prefix = file.name.slice(0, 10)
         if (!DATE_REGEX.test(prefix)) {
             continue
         }
 
-        if (file.isFile && !isNaN(+prefix[0]) && prefix !== TODAY_YYYY_MM_DD) {
+        if (file.isFile && !isNaN(+prefix[0]) && isOldCache(prefix)) {
             const filePath = `${CACHE_DIR}/${file.name}`
             console.log(`Deleting old cache file: ${filePath}`)
-            await Deno.remove(filePath)
+            // await Deno.remove(filePath)
         }
     }
 
-    // await removeBustagoCaches()
-
     console.log(`Cache directory cleaned up. Old caches removed.`)
+}
+
+function isOldCache(yyyymmdd: string) {
+    const today = new Date()
+    const cacheDate = new Date(yyyymmdd)
+    cacheDate.setHours(0, 0, 0, 0) // Set time to midnight
+    today.setHours(0, 0, 0, 0) // Set time to midnight
+
+    return cacheDate < today
+}
+
+if (import.meta.main) {
+    await removeOldCaches()
 }
