7afeff964eb08feae2d57e074101a956a2bbd3a9
rycont <reactdev@kakao.com>
2025-06-20 00:24:08 +0900

    fix: 오류가 나도 그냥 실행하는 캐시 리무버

--- remove-old-caches.ts
+++ remove-old-caches.ts
@@ -6,32 +6,37 @@ const CACHE_DIR = Deno.env.get('CACHE_DIR') || './cache'
 export default async function removeOldCaches() {
     // 생성된 날짜가 오늘 날짜보다 이전인 캐시 파일 삭제
     for await (const file of Deno.readDir(CACHE_DIR)) {
-        const content = await Deno.readTextFile(`${CACHE_DIR}/${file.name}`)
+        try {
+            const content = await Deno.readTextFile(`${CACHE_DIR}/${file.name}`)
 
-        const parsedCache = cacheV2Scheme.safeParse(JSON.parse(content))
+            const parsedCache = cacheV2Scheme.safeParse(JSON.parse(content))
 
-        if (parsedCache.success) {
-            // Is cache V2
-            const expires = new Date(parsedCache.data.expires)
-            const hasExpired = expires < new Date()
+            if (parsedCache.success) {
+                // Is cache V2
+                const expires = new Date(parsedCache.data.expires)
+                const hasExpired = expires < new Date()
 
-            if (hasExpired) {
-                const filePath = `${CACHE_DIR}/${file.name}`
-                await Deno.remove(filePath)
+                if (hasExpired) {
+                    const filePath = `${CACHE_DIR}/${file.name}`
+                    await Deno.remove(filePath)
+                }
+            } else {
+                const prefix = file.name.slice(0, 10)
+                // Is cache V1
+                if (!DATE_REGEX.test(prefix)) {
+                    console.warn(`Invalid cache file name: ${file.name}`)
+                }
+
+                // Check if the prefix is a valid date
+
+                if (file.isFile && !isNaN(+prefix[0]) && isOldCache(prefix)) {
+                    const filePath = `${CACHE_DIR}/${file.name}`
+                    await Deno.remove(filePath)
+                }
             }
-        } else {
-            const prefix = file.name.slice(0, 10)
-            // Is cache V1
-            if (!DATE_REGEX.test(prefix)) {
-                console.warn(`Invalid cache file name: ${file.name}`)
-            }
-
-            // Check if the prefix is a valid date
-
-            if (file.isFile && !isNaN(+prefix[0]) && isOldCache(prefix)) {
-                const filePath = `${CACHE_DIR}/${file.name}`
-                await Deno.remove(filePath)
-            }
+        } catch (error) {
+            console.error(`Error processing file ${file.name}:`, error)
+            continue
         }
     }
 
