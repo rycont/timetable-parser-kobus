d4c099a83f396bcd162244f03dceac54e90a32c4
정한 Rycont <35295182+rycont@users.noreply.github.com>
2025-06-21 08:57:50 +0900

    chore: sync submodules

--- cache-tester.ts
+++
@@ -1,8 +0,0 @@
-import { getTerminals as getBustagoTerminals } from './bustago/get-terminals.ts'
-import { getRoutesFromTerminal as getBustagoRoutesFromTerminal } from './bustago/get-routes-from-terminal.ts'
-import { getPlansFromRoute as getBustagoPlansFromRoute } from './bustago/get-plans-from-route.ts'
-
-const depart = (await getBustagoTerminals()).values().next().value!
-const arrive = (await getBustagoRoutesFromTerminal(depart.id)).data[1]
-
-await getBustagoPlansFromRoute(depart.id, arrive.id)
